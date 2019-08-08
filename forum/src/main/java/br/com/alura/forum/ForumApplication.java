0417cf7362fceafea625ac701476474a6a46fed6
Jorge Hecherat <hecherat@gmail.com>
2019-08-08 20:36:38 -0300

    habilitando o uso de cache na aplicação

--- forum/src/main/java/br/com/alura/forum/ForumApplication.java
+++ forum/src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,6 +2,7 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 /*
@@ -10,10 +11,13 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
  * 'EnableSpringDataWebSupport' Habilitando suporte para o Spring pegar 
  * da requisição os campos de ordenação e páginação e repassar isso para o 
  * Spring Data
+ * 
+ * 'EnableCaching' Habilita o uso de cache na nossa aplicação
  */
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
+@EnableCaching
 public class ForumApplication {
 
 	public static void main(String[] args) {
