d43eb5e7d9805ac032a303a8b979934c06fbe992
Jorge Hecherat <hecherat@gmail.com>
2019-08-08 12:08:17 -0300

    funcionalidade Atualizar tópicos

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -3,13 +3,16 @@ package br.com.alura.forum.controller;
 import java.net.URI;
 import java.util.List;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +20,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.controller.dto.DetalhesDoTopicoDto;
 import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.controller.form.AtualizacaoTopicoForm;
 import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.model.Topico;
 import br.com.alura.forum.repository.CursoRepository;
@@ -80,6 +84,20 @@ public class TopicosController {
 	public DetalhesDoTopicoDto detalhar(@PathVariable Long id) {
 		Topico topico = topicoRepository.getOne(id);	// getOne() do Spring já busca o id
 		return new DetalhesDoTopicoDto(topico);
+	}
+	
+	/* 
+	 * Para atualizações as annotations podem ser 'PutMapping' ou 'PatchMapping'
+	 * Put: para sobrescrever o recurso inteiro, mudar todas as informações.
+	 * Patch: pequena att, apenas alguns campos.
+	 */
+	@PutMapping("/{id}")
+	@Transactional	//'Trasational'avisa o Spring que precisa commitar este método quando for executado
+	public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, 
+			@RequestBody @Valid AtualizacaoTopicoForm form) {
+		Topico topico = form.atualizar(id, topicoRepository);
+		
+		return ResponseEntity.ok(new TopicoDto(topico));
 	}
 	
 }
